d8c31e9c5f500592673fb096074de58769e5a67b
meergit1 <meeralrafee57@gmail.com>
2024-10-17 21:59:16 -0400

    Add files via upload

---
+++ js02/project03/project02-03.js
@@ -0,0 +1,38 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-03
+
+      Application to return the shape of a clicked object
+      Author: Meer Alrafee
+      Date:   10/08/24
+
+      Filename: project02-03.js
+ */
+let square = document.getElementById("square");
+let triangle = document.getElementById("triangle");
+let circle = document.getElementById("circle");
+let feedback = document.getElementById("feedback");
+
+square.onmouseover = function() {
+    feedback.innerHTML = "You're hovering over the square";
+};
+
+square.onmouseout = function() {
+    feedback.innerHTML = "";
+};
+
+triangle.onmouseover = function() {
+    feedback.innerHTML = "You're hovering over the triangle";
+};
+
+triangle.onmouseout = function() {
+    feedback.innerHTML = "";
+};
+
+circle.onmouseover = function() {
+    feedback.innerHTML = "You're hovering over the circle";
+};
+
+circle.onmouseout = function() {
+    feedback.innerHTML = "";
+};
